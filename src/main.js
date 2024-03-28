fae0d31547244183e17d4971f19aae237914ae0f
Nguyen Thu <thub2014706@student.ctu.edu.vn>
2024-03-28 23:07:24 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import "@fortawesome/fontawesome-free"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
+
+// createApp(App).mount('#app')
